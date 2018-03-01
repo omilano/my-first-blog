ff0a23167bb779fdccee900fc0f753c9ab0c7076
omilano <orlandomilano@gmail.com>
2018-03-01 09:18:33 -0430

    jueves 1 de marzo 2018 1er

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class DeleteNewForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = []        
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, DeleteNewForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -37,4 +37,15 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-#                 
+#
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = DeleteNewForm(request.POST, instance=post)
+        if form.is_valid():
+#			post.delete()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = DeleteNewForm(instance=post)
+    return render(request, 'blog/post_delete.html', {'form': form})
